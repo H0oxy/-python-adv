73153b76be1802c63fbff6610c8761350b1c6573
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-02-21 21:16:18 +0500

    4

--- 21.02.15/realpython_hometask.py
+++ 21.02.15/realpython_hometask.py
@@ -35,14 +35,23 @@
 
 
 
-def my_decorator(func):
-    def wrapper():
-        print("Something is happening before the function is called.")
-        func()
-        print("Something is happening after the function is called.")
-    return wrapper
+# def my_decorator(func):
+#     def wrapper():
+#         print("Something is happening before the function is called.")
+#         func()
+#         print("Something is happening after the function is called.")
+#     return wrapper
+#
+# @my_decorator
+# def say_whee():
+#     print("Whee!")
+# say_whee()
 
-@my_decorator
+
+
+from decorators import do_twice
+
+@do_twice
 def say_whee():
     print("Whee!")
 say_whee()
