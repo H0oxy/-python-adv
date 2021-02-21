ff04d002f6d6f97035bfe902d0c0a948f5fa0afa
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-02-21 21:15:58 +0500

    3

--- 21.02.15/realpython_hometask.py
+++ 21.02.15/realpython_hometask.py
@@ -16,19 +16,33 @@
 
 
 
-from datetime import datetime
+# from datetime import datetime
+#
+# def not_during_the_night(func):
+#     def wrapper():
+#         if 7 <= datetime.now().hour < 22:
+#             func()
+#         else:
+#             pass  # Hush, the neighbors are asleep
+#     return wrapper
+#
+# def say_whee():
+#     print("Whee!")
+#
+# say_whee = not_during_the_night(say_whee)
+#
+# say_whee()
 
-def not_during_the_night(func):
+
+
+def my_decorator(func):
     def wrapper():
-        if 7 <= datetime.now().hour < 22:
-            func()
-        else:
-            pass  # Hush, the neighbors are asleep
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
     return wrapper
 
+@my_decorator
 def say_whee():
     print("Whee!")
-
-say_whee = not_during_the_night(say_whee)
-
 say_whee()
