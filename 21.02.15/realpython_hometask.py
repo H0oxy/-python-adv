6e995e606e3c7839088ef258caaecb9124e8b783
H0oxy <xMolod0yx.Arseny@yandex.ru>
2021-02-21 21:16:38 +0500

    5

--- 21.02.15/realpython_hometask.py
+++ 21.02.15/realpython_hometask.py
@@ -55,3 +55,13 @@ from decorators import do_twice
 def say_whee():
     print("Whee!")
 say_whee()
+
+
+
+
+from decorators import do_twice
+
+@do_twice
+def greet(name):
+    print(f"Hello {name}")
+greet("World")
